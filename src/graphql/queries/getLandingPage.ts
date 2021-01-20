b0c7ac4610486178a625a236b986b8182f5e375b
theandersonn <theandersonn@gmail.com>
2021-01-20 10:27:14 -0300

    sectionFaq dinamico

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -119,6 +119,16 @@ const GET_LANDING_PAGE = /* GraphQL */ `
     }
   }
 
+  fragment sectionFaq on LandingPage {
+    sectionFaq {
+      title
+      questions {
+        question
+        answer
+      }
+    }
+  }
+
   query GET_LANDING_PAGE {
     landingPage {
       ...logo
@@ -131,6 +141,7 @@ const GET_LANDING_PAGE = /* GraphQL */ `
       ...pricingBox
       ...sectionAboutUs
       ...sectionReviews
+      ...sectionFaq
     }
   }
 `
